12eece79aa713c4c697d33e765dbf5923a18cc54
Gheorghe Birau <gheorghe.birau@yahoo.com>
2023-12-05 10:42:42 +0200

    First commit

---
+++ src/main/java/com/utcn/projectRC/ProjectRcApplication.java
@@ -0,0 +1,13 @@
+package com.utcn.projectRC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRcApplication.class, args);
+	}
+
+}
